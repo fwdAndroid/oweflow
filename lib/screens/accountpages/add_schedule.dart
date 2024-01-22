8ee06678afc1a0915a5feb5f969e5badce72723e
fawad kaleem <fwdkaleem@gmail.com>
2024-01-22 07:28:45 +0500

    Remainders

--- lib/screens/accountpages/add_schedule.dart
+++ lib/screens/accountpages/add_schedule.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:oweflow/screens/accountpages/add_contact.dart';
 import 'package:oweflow/screens/accountpages/show_contact.dart';
 import 'package:oweflow/utils/colors.dart';
+import 'package:intl/intl.dart';
 
 class AddSchedules extends StatefulWidget {
   const AddSchedules({super.key});
@@ -12,6 +12,22 @@ class AddSchedules extends StatefulWidget {
 }
 
 class _AddSchedulesState extends State<AddSchedules> {
+  TextEditingController _emailController = TextEditingController();
+  String dropdownValue = "Does not repeat";
+  String remainders = "Disabled";
+  List<String> listRecrudesce = <String>[
+    'Does not repeat',
+    'Every 2 weeks',
+    'Every month',
+  ];
+  List<String> listRemainders = <String>[
+    'Disabled',
+    'Weekly',
+    'Bi-Weekly',
+    'Monthly'
+  ];
+  TextEditingController _dateController = TextEditingController();
+  TextEditingController _notesController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -101,14 +117,26 @@ class _AddSchedulesState extends State<AddSchedules> {
               padding: const EdgeInsets.all(8.0),
               child: Align(
                 alignment: AlignmentDirectional.topStart,
-                child: Text(
-                  '\$2460',
-                  textAlign: TextAlign.center,
-                  style: GoogleFonts.inter(
-                    color: Colors.white,
-                    fontSize: 22,
-                    fontWeight: FontWeight.w600,
-                    height: 0,
+                child: Container(
+                  width: 130,
+                  child: TextFormField(
+                    keyboardType: TextInputType.number,
+                    controller: _emailController,
+                    style: GoogleFonts.dmSans(color: colorwhite),
+                    decoration: InputDecoration(
+                        enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: borderColor)),
+                        errorBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: borderColor)),
+                        focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: borderColor)),
+                        border: OutlineInputBorder(
+                            borderSide: BorderSide(color: borderColor)),
+                        hintText: "\$600",
+                        hintStyle: GoogleFonts.inter(
+                            color: colorwhite,
+                            fontSize: 22,
+                            fontWeight: FontWeight.w600)),
                   ),
                 ),
               ),
@@ -120,6 +148,7 @@ class _AddSchedulesState extends State<AddSchedules> {
               margin: const EdgeInsets.only(left: 10, right: 10),
               padding: const EdgeInsets.all(8),
               child: TextFormField(
+                controller: _notesController,
                 maxLines: 3,
                 style: GoogleFonts.dmSans(color: colorwhite),
                 decoration: InputDecoration(
@@ -136,33 +165,61 @@ class _AddSchedulesState extends State<AddSchedules> {
                         GoogleFonts.dmSans(color: colorwhite, fontSize: 12)),
               ),
             ),
-            Container(
-              margin: const EdgeInsets.only(left: 10, right: 10),
-              padding: const EdgeInsets.all(8),
-              child: TextFormField(
-                style: GoogleFonts.dmSans(color: colorwhite),
-                decoration: InputDecoration(
-                    suffixIcon: Icon(
-                      Icons.arrow_drop_down,
-                      color: colorwhite,
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: borderColor)),
-                    errorBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: borderColor)),
-                    focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: borderColor)),
-                    border: OutlineInputBorder(
-                        borderSide: BorderSide(color: borderColor)),
-                    hintText: "Recurrence",
-                    hintStyle:
-                        GoogleFonts.dmSans(color: colorwhite, fontSize: 12)),
-              ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  margin: EdgeInsets.only(bottom: 10, left: 18),
+                  child: Text(
+                    "Recurrence",
+                    style: GoogleFonts.inter(color: colorwhite),
+                  ),
+                ),
+                Container(
+                    decoration:
+                        BoxDecoration(border: Border.all(color: borderColor)),
+                    margin: const EdgeInsets.only(left: 18, right: 18),
+                    padding: const EdgeInsets.all(8),
+                    child: DropdownButton<String>(
+                      isExpanded: true,
+                      value: dropdownValue,
+                      icon: Icon(
+                        Icons.arrow_drop_down,
+                        color: colorwhite,
+                      ),
+                      dropdownColor: Colors.transparent,
+                      style: TextStyle(color: Colors.black),
+                      elevation: 16,
+                      underline: Container(
+                        height: 0,
+                      ),
+                      onChanged: (String? value) {
+                        // This is called when the user selects an item.
+                        setState(() {
+                          dropdownValue = value!;
+                        });
+                      },
+                      items: listRecrudesce
+                          .map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(
+                            value,
+                            style: TextStyle(color: colorwhite),
+                          ),
+                        );
+                      }).toList(),
+                    )),
+              ],
             ),
             Container(
               margin: const EdgeInsets.only(left: 10, right: 10),
               padding: const EdgeInsets.all(8),
               child: TextFormField(
+                onTap: () {
+                  _selectDate(); // Call Function that has showDatePicker()
+                },
+                controller: _dateController,
                 style: GoogleFonts.dmSans(color: colorwhite),
                 decoration: InputDecoration(
                     suffixIcon: Icon(
@@ -182,28 +239,52 @@ class _AddSchedulesState extends State<AddSchedules> {
                         GoogleFonts.dmSans(color: colorwhite, fontSize: 12)),
               ),
             ),
-            Container(
-              margin: const EdgeInsets.only(left: 10, right: 10),
-              padding: const EdgeInsets.all(8),
-              child: TextFormField(
-                style: GoogleFonts.dmSans(color: colorwhite),
-                decoration: InputDecoration(
-                    suffixIcon: Icon(
-                      Icons.arrow_drop_down,
-                      color: colorwhite,
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: borderColor)),
-                    errorBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: borderColor)),
-                    focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: borderColor)),
-                    border: OutlineInputBorder(
-                        borderSide: BorderSide(color: borderColor)),
-                    hintText: "Remainders",
-                    hintStyle:
-                        GoogleFonts.dmSans(color: colorwhite, fontSize: 12)),
-              ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  margin: EdgeInsets.only(bottom: 10, left: 18),
+                  child: Text(
+                    "Remainders",
+                    style: GoogleFonts.inter(color: colorwhite),
+                  ),
+                ),
+                Container(
+                    decoration:
+                        BoxDecoration(border: Border.all(color: borderColor)),
+                    margin: const EdgeInsets.only(left: 18, right: 18),
+                    padding: const EdgeInsets.all(8),
+                    child: DropdownButton<String>(
+                      isExpanded: true,
+                      value: remainders,
+                      icon: Icon(
+                        Icons.arrow_drop_down,
+                        color: colorwhite,
+                      ),
+                      dropdownColor: Colors.transparent,
+                      style: TextStyle(color: Colors.black),
+                      elevation: 16,
+                      underline: Container(
+                        height: 0,
+                      ),
+                      onChanged: (String? value) {
+                        // This is called when the user selects an item.
+                        setState(() {
+                          remainders = value!;
+                        });
+                      },
+                      items: listRemainders
+                          .map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(
+                            value,
+                            style: TextStyle(color: colorwhite),
+                          ),
+                        );
+                      }).toList(),
+                    )),
+              ],
             ),
             Container(
               margin: EdgeInsets.only(top: 20),
@@ -238,5 +319,21 @@ class _AddSchedulesState extends State<AddSchedules> {
         ),
       ),
     );
+  }
+
+  void _selectDate() async {
+    DateTime? pickedDate = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2101),
+    );
+
+    if (pickedDate != null && pickedDate != DateTime.now()) {
+      // Update the text field with the selected date
+      setState(() {
+        _dateController.text = DateFormat('yyyy-MM-dd').format(pickedDate);
+      });
+    }
   }
 }
