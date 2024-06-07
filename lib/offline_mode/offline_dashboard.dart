4aae8f436497eadd83108f654577ca89e2fb03c4
fwdAndroid <fwdkaleem@gmail.com>
2024-06-08 03:43:18 +0500

    Selection Completed

---
+++ lib/offline_mode/offline_dashboard.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class OfflineDashboard extends StatefulWidget {
+  const OfflineDashboard({super.key});
+
+  @override
+  State<OfflineDashboard> createState() => _OfflineDashboardState();
+}
+
+class _OfflineDashboardState extends State<OfflineDashboard> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/selection_screen.dart
+++ lib/selection_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:oweflow/offline_mode/offline_dashboard.dart';
 import 'package:oweflow/screens/auth/sign_in_page.dart';
 import 'package:oweflow/utils/buttons.dart';
 import 'package:oweflow/utils/colors.dart';
@@ -15,6 +16,7 @@ class SelectionScreen extends StatefulWidget {
 
 class _SelectionScreenState extends State<SelectionScreen> {
   SingingCharacter? _character = SingingCharacter.lafayette;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -43,7 +45,7 @@ class _SelectionScreenState extends State<SelectionScreen> {
                       color: black, fontWeight: FontWeight.w500, fontSize: 19),
                 ),
                 subtitle: Text(
-                  "Use this mode to access your data across devices, receive real-time notifications, and enjoy cloud backup and synchronization.  Your data will be securely stored on our servers, ensuring accessibility and convenience.",
+                  "Use this mode to access your data across devices, receive real-time notifications, and enjoy cloud backup and synchronization. Your data will be securely stored on our servers, ensuring accessibility and convenience.",
                   style: GoogleFonts.plusJakartaSans(
                       color: black, fontWeight: FontWeight.w400, fontSize: 13),
                 ),
@@ -63,7 +65,7 @@ class _SelectionScreenState extends State<SelectionScreen> {
                     color: black, fontWeight: FontWeight.w500, fontSize: 19),
               ),
               subtitle: Text(
-                "Choose this mode for enhanced privacy and to keep your   data exclusively on your device. You won't receive real-time notifications, but you can manage your transactions and profiles locally, ensuring  complete control over your information.",
+                "Choose this mode for enhanced privacy and to keep your data exclusively on your device. You won't receive real-time notifications, but you can manage your transactions and profiles locally, ensuring complete control over your information.",
                 style: GoogleFonts.plusJakartaSans(
                     color: black, fontWeight: FontWeight.w400, fontSize: 13),
               ),
@@ -78,11 +80,22 @@ class _SelectionScreenState extends State<SelectionScreen> {
             Flexible(child: Container()),
             Center(
               child: SaveButton(
-                  title: "Continue",
-                  onTap: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (builder) => SignInPage()));
-                  }),
+                title: "Continue",
+                onTap: () {
+                  if (_character == SingingCharacter.lafayette) {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (builder) => SignInPage()),
+                    );
+                  } else if (_character == SingingCharacter.jefferson) {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (builder) => OfflineDashboard()),
+                    );
+                  }
+                },
+              ),
             ),
             const SizedBox(
               height: 20,
