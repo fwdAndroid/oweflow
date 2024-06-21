19f00186c61f6865249fdd987465a59eac50d381
fwdAndroid <fwdkaleem@gmail.com>
2024-06-21 20:11:02 +0500

    Edit Lend

--- lib/screens/pages/tab_pages/lenders.dart
+++ lib/screens/pages/tab_pages/lenders.dart
@@ -3,7 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:oweflow/screens/accountpages/personalprofile.dart';
-import 'package:oweflow/screens/accountpages/premium_features.dart';
+import 'package:oweflow/screens/pages/tab_pages/edit_lend_page.dart';
 import 'package:oweflow/utils/colors.dart';
 
 class Lenders extends StatefulWidget {
@@ -28,27 +28,19 @@ class _LendersState extends State<Lenders> {
       appBar: AppBar(
         actions: [
           IconButton(
-              onPressed: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (builder) => PersonalProfile()));
-              },
-              icon: Icon(
-                Icons.settings,
-                color: black,
-              )),
-        ],
-        leading: IconButton(
             onPressed: () {
               Navigator.push(context,
-                  MaterialPageRoute(builder: (builder) => PremiumFeatures()));
+                  MaterialPageRoute(builder: (builder) => PersonalProfile()));
             },
             icon: Icon(
-              Icons.menu,
+              Icons.settings,
               color: black,
-            )),
+            ),
+          ),
+        ],
         centerTitle: true,
         title: Text(
-          "Home",
+          "Lenders",
           style: GoogleFonts.plusJakartaSans(
               color: black, fontWeight: FontWeight.w500, fontSize: 16),
         ),
@@ -103,7 +95,7 @@ class _LendersState extends State<Lenders> {
           ),
           SizedBox(
             width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height,
+            height: MediaQuery.of(context).size.height / 1.5,
             child: StreamBuilder(
                 stream: FirebaseFirestore.instance
                     .collection("debitTransaction")
@@ -135,36 +127,73 @@ class _LendersState extends State<Lenders> {
 
                         return Dismissible(
                           key: Key(documents[index].id),
-                          background: Container(color: Colors.red),
-                          confirmDismiss: (direction) async {
-                            return await showDialog(
-                              context: context,
-                              builder: (BuildContext context) {
-                                return AlertDialog(
-                                  title: Text("Confirm"),
-                                  content: Text(
-                                      "Are you sure you wish to delete this item?"),
-                                  actions: <Widget>[
-                                    TextButton(
-                                      onPressed: () =>
-                                          Navigator.of(context).pop(false),
-                                      child: Text("CANCEL"),
-                                    ),
-                                    TextButton(
-                                      onPressed: () async {
-                                        await FirebaseFirestore.instance
-                                            .collection("debitTransaction")
-                                            .doc(documents[index].id)
-                                            .delete();
-                                        Navigator.of(context).pop(true);
-                                      },
-                                      child: Text("DELETE"),
-                                    ),
-                                  ],
-                                );
-                              },
-                            );
+                          confirmDismiss: (DismissDirection direction) async {
+                            if (direction == DismissDirection.endToStart) {
+                              return await showDialog(
+                                context: context,
+                                builder: (BuildContext context) {
+                                  return AlertDialog(
+                                    title: Text("Confirm"),
+                                    content: Text(
+                                        "Are you sure you want to delete this item?"),
+                                    actions: <Widget>[
+                                      TextButton(
+                                        onPressed: () =>
+                                            Navigator.of(context).pop(false),
+                                        child: Text("Cancel"),
+                                      ),
+                                      TextButton(
+                                        onPressed: () =>
+                                            Navigator.of(context).pop(true),
+                                        child: Text("Delete"),
+                                      ),
+                                    ],
+                                  );
+                                },
+                              );
+                            } else if (direction ==
+                                DismissDirection.startToEnd) {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) =>
+                                      EditLendPage(document: data),
+                                ),
+                              );
+                              return false;
+                            }
+                            return false;
                           },
+                          onDismissed: (DismissDirection direction) async {
+                            if (direction == DismissDirection.endToStart) {
+                              await FirebaseFirestore.instance
+                                  .collection('debitTransaction')
+                                  .doc(documents[index].id)
+                                  .delete();
+                              setState(() {
+                                documents.removeAt(index);
+                              });
+                            }
+                          },
+                          background: Container(
+                              color: Colors.green,
+                              child: Align(
+                                alignment: Alignment.centerLeft,
+                                child: Padding(
+                                  padding: const EdgeInsets.only(left: 16.0),
+                                  child: Icon(Icons.edit, color: Colors.white),
+                                ),
+                              )),
+                          secondaryBackground: Container(
+                              color: Colors.red,
+                              child: Align(
+                                alignment: Alignment.centerRight,
+                                child: Padding(
+                                  padding: const EdgeInsets.only(right: 16.0),
+                                  child:
+                                      Icon(Icons.delete, color: Colors.white),
+                                ),
+                              )),
                           child: Column(
                             children: [
                               ListTile(
@@ -186,19 +215,6 @@ class _LendersState extends State<Lenders> {
                                         fontSize: 24,
                                         fontWeight: FontWeight.w400,
                                       ),
-                                    ),
-                                    IconButton(
-                                      icon:
-                                          Icon(Icons.edit, color: Colors.blue),
-                                      onPressed: () {
-                                        Navigator.push(
-                                          context,
-                                          MaterialPageRoute(
-                                            builder: (context) =>
-                                                EditPage(document: data),
-                                          ),
-                                        );
-                                      },
                                     ),
                                   ],
                                 ),
@@ -266,24 +282,5 @@ class _LendersState extends State<Lenders> {
     } catch (e) {
       print('Error calculating total amount: $e');
     }
-  }
-}
-
-class EditPage extends StatelessWidget {
-  final Map<String, dynamic> document;
-
-  EditPage({required this.document});
-
-  @override
-  Widget build(BuildContext context) {
-    // Implement your edit page here
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Edit Transaction'),
-      ),
-      body: Center(
-        child: Text('Edit data for ${document['contact']}'),
-      ),
-    );
   }
 }
